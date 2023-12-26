1366b2ea4c1de766eeffb5112dfe87024a87be85
moisamidi <moisamidi.com>
2023-12-26 18:15:05 +0200

    start

--- src/main/java/ua/ucu/edu/apps/task3/Main.java
+++ src/main/java/ua/ucu/edu/apps/task3/Main.java
@@ -6,6 +6,6 @@ public class Main {
     @SneakyThrows
     public static void main(String[] args) {
         PDLReader pdl = new PDLReader();
-        pdl.parse("https://github.com/SahaidakYurii/oop-lab13");
+        pdl.parse("https://github.com/moisamidi/lab13");
     }
 }
